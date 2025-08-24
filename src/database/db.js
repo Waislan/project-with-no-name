a2d1b4db538872223431b3cbdeb4b212562239e0
Waislan Sanches <waislanluis@gmail.com>
2025-08-24 20:19:47 -0300

    infra: changes db connection to transaction pooler

--- src/database/db.js
+++ src/database/db.js
@@ -4,14 +4,13 @@ const { Pool } = pkg;
 export async function connectToDatabase(config = {}) {
     try {
         const pool = new Pool({
-            connectionString: process.env.DB_URL,
+            //connectionString: process.env.DB_URL,
             ssl: { rejectUnauthorized: false },
-            host: process.env.DB_HOST || 'localhost',
-            port: process.env.DB_PORT || 5432,
-            family: 4,
-            // user: process.env.DB_USER || 'postgres',
-            // password: process.env.DB_PASSWORD || 'postgres',
-            // database: process.env.DB_DATABASE || 'minha_api',
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE,
             // max: 10,
             // idleTimeoutMillis: 30000,
             // ...config,
